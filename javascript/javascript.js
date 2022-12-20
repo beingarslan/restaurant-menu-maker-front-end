7cab01a2ba9a44c1401027e15a224cd48971cb5a
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-12-21 01:37:49 +0500

    changes some

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -5,18 +5,3 @@ function myFunction() {
     var element = document.getElementById("cardsmed");
     element.classList.toggle("d-none");
  }
-
-// // Path: javascript\javascript.js
-// function myFunction() {
-//     var element = document.getElementById("cardslar");
-//     element.classList.toggle("d-none");
-//  }
-// one function in multy pal card in one page
-
-// using add and remove class in javascript
-// // Path: javascript\javascript.js
-// function myFunction() {
-//     var element = document.getElementById("cardsmed");
-//     element.classList.add("d-none");
-//     element.classList.remove("d-none");
-//  }
