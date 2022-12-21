b38047eb4ad4cbad022b31634c6268f92fef2a67
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-12-21 13:52:23 +0500

    changing continue jQuery toggle btn

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,7 +1,7 @@
 
 
-// Path: javascript\javascript.js
-function myFunction() {
-    var element = document.getElementById("cardsmed");
-    element.classList.toggle("d-none");
- }
+// // Path: javascript\javascript.js
+// function myFunction() {
+//     var element = document.getElementById("cardsmed");
+//     element.classList.toggle("d-none");
+//  }
